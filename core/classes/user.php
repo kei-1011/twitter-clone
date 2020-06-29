e5fdc0adec372b7eb7bd7a8a77de8670c408cf10
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-06-29 22:53:01 +0900

    ユーザー情報の更新

--- core/classes/user.php
+++ core/classes/user.php
@@ -77,6 +77,26 @@ class User {
 		}
 	}
 
+  public function update($table, $user_id, $fields = array()) {
+    $columns  = '';
+    $i        = 1;
+
+  foreach($fields as $name => $value) {
+    $columns .= "`{$name}` = :{$name}";
+    if($i < count($fields)) {
+      $columns .= '. ';
+    }
+  }
+
+  $sql = "UPDATE {$table} SET {$columns} where `user_id` = {$user_id}";
+  if($stmt = $this->pdo->prepare($sql)) {
+    foreach($fields as $key => $value) {
+      $stmt->bindValue(':'.$key, $value);
+    }
+    $stmt->execute();
+  }
+}
+
   // メールアドレスの存在チェック
   public function checkEmail($email) {
     $stmt = $this->pdo->prepare("SELECT email FROM users WHERE email = :email");
